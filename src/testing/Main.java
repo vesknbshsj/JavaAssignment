dd1a372c41db3b4c8ef5c620f4c41150d248c8cd
veson <veson@172.16.138.50>
2025-03-13 10:08:17 +0800

    t1

--- src/testing/Main.java
+++ src/testing/Main.java
@@ -3,6 +3,8 @@ package testing;
 public class Main {
 	public static void main(String[] args) {
 		Testing t = new Testing();
+	
 		t.displayTesting();
-	}
+		Testing t1 = new Testing();
+		t1.displayTesting();	}
 }
